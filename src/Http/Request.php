f7c509a98e1c7675c9a73fb898373b02c9267043
REandH <reandh@yandex.ru>
2021-11-19 20:31:14 +0500

    Test 002

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -78,9 +78,11 @@ class Request
     {
         // TODO Rafikov Move it to another place
         $baseRequestUri = $_SERVER['REQUEST_URI'];
-        if($_COOKIE['AUTH'] !== 'TRUE' && $baseRequestUri == '/public') {
-            $baseRequestUri = 'public/Auth';
+        /*
+        if($_COOKIE['AUTH'] !== 'TRUE' && $baseRequestUri == '/') {
+            $baseRequestUri = 'Auth';
         }
+        */
         return $this->parse(
             $_SERVER['HTTP_HOST'], $baseRequestUri, $_GET, $_POST,
         );
