e9439f2e8383ec1fe71e22036cd44ec2cd228426
REandH <reandh@yandex.ru>
2021-11-19 21:19:07 +0500

    Test 009

--- src/setup.php
+++ src/setup.php
@@ -1,7 +1,7 @@
 <?php
 
 /** Absolute path to project root. */
-define('PROJECT_ROOT', dirname(__DIR__));
+define('PROJECT_ROOT', '/var/www'.dirname(__DIR__));
 
 require_once __DIR__.'/Conf.php';
 \Chat\Conf::parseFromFile();
